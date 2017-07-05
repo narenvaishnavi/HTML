ecf9bbb8a8799e847f78127def49ce67142f5cfa
narenvaishnavi <narenvaishnavi@users.noreply.github.com>
2017-07-05 15:30:13 -0400

    Add files via upload

---
+++ Food_Delivery/public_html/js/index.js
@@ -0,0 +1,20 @@
+$(function () {
+  $('[data-toggle="popover"]').popover();
+  
+  $('#cvc').on('click', function(){
+    if ( $('.cvc-preview-container').hasClass('hide') ) {
+      $('.cvc-preview-container').removeClass('hide');
+    } else {
+      $('.cvc-preview-container').addClass('hide');
+    }    
+  });
+  
+  $('.cvc-preview-container').on('click', function(){
+    $(this).addClass('hide');
+  });
+  
+   $('PayButton').on('click',function(){
+       window.location = "payment.html";
+   });
+});
+
